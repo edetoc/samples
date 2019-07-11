bd15dd1d2746c7ef5573345391e62ab5c5889024
Eric Detoc <e_detoc@hotmail.com>
2019-07-11 07:52:38 +0200

    added GPO sample

---
+++ GPOSample/GPOSample/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.GroupPolicy;
+
+namespace GPOSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try 
+            {
+                var domain = new GPDomain("edetoc.lab");
+
+                var gpo = domain.GetGpo("xxxGPO");
+                var somcollection = domain.SearchSoms (gpo);
+
+                foreach (var som in somcollection)
+                {
+                    //Console.WriteLine(som.Name);
+                    //Console.WriteLine(som.Path);
+
+                    foreach (var link in som.GpoLinks )
+                    {
+                        if (Guid.Equals(gpo.Id, link.GpoId))
+                        {
+                            Console.WriteLine("Need to suppress link to "
+                                                    + link.DisplayName // GPO name
+                                                    + " in "
+                                                    + link.Target);    // path
+
+                            link.Delete();
+                            Console.WriteLine("link suppressed");
+
+                        }
+                        
+                    }
+                }
+
+                // We have suppressed all links to the GPO in the domain
+                // we can now suppress the GPO object itself
+
+                gpo.Delete ();
+                Console.WriteLine("xxxGPO deleted with success.");
+            
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            
+        }
+    }
+}
