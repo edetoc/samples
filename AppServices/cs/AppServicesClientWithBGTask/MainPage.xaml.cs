32a0283fb38713ab9a0114fbc0aed6e1b6dd8cef
Eric Detoc <e_detoc@hotmail.com>
2019-07-14 12:32:41 +0200

    added Appservices sample

---
+++ AppServices/cs/AppServicesClientWithBGTask/MainPage.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Background;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AppServicesClientWithBGTask
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        ObservableCollection<string> logItems;
+
+        public MainPage()
+        {
+            logItems = new ObservableCollection<string>();
+
+            this.InitializeComponent();
+            
+            LogLv.ItemsSource = logItems;
+        }
+
+        private async void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            var taskRegistered = false;
+            var taskName = "SampleBackgroundTask";
+
+            foreach (var cur in BackgroundTaskRegistration.AllTasks)
+            {
+                if (cur.Value.Name == taskName)
+                {
+                    taskRegistered = true;
+                    break;
+                }
+            }
+
+            if (taskRegistered)
+            {
+                logItems.Add("task already registered");
+                return;
+            }
+
+            logItems.Add("task not found, let's create it");
+            // Create background task
+
+            var requestStatus = await BackgroundExecutionManager.RequestAccessAsync();
+            if ((requestStatus == BackgroundAccessStatus.DeniedBySystemPolicy)
+                || (requestStatus == BackgroundAccessStatus.DeniedByUser))
+            {
+                logItems.Add(string.Format("RequestAccessAsync failed: {0}", requestStatus.ToString()));
+                return;
+            }
+
+            logItems.Add("RequestAccessAsync succeeded");
+
+            var builder = new BackgroundTaskBuilder();
+
+            builder.Name = "SampleBackgroundTask";
+            builder.TaskEntryPoint = "BGTaskRuntimeComponent.SampleBackgroundTask";
+            builder.SetTrigger(new TimeTrigger(15, false)); // periodic every 15 minutes
+
+
+            // register background task
+            BackgroundTaskRegistration task = builder.Register();
+
+            logItems.Add("Background task registered");
+
+            AttachCompletedHandler(task);
+
+        }
+
+        private void UnregisterButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            var taskName = "SampleBackgroundTask";
+
+            foreach (var cur in BackgroundTaskRegistration.AllTasks)
+            {
+                if (cur.Value.Name == taskName)
+                {
+                    cur.Value.Unregister(true);
+                    logItems.Add("Background task unregistered");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Attach progress and completed handers to a background task.
+        /// </summary>
+        /// <param name="task">The task to attach progress and completed handlers to.</param>
+        private void AttachCompletedHandler(IBackgroundTaskRegistration task)
+        {
+            //task.Progress += new BackgroundTaskProgressEventHandler(OnProgress);
+            task.Completed += new BackgroundTaskCompletedEventHandler(OnCompleted);
+        }
+
+        private async void OnCompleted(BackgroundTaskRegistration sender, BackgroundTaskCompletedEventArgs args)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+           () =>
+           {
+                //RegisterButton.IsEnabled = !BackgroundTaskSample.TimeTriggeredTaskRegistered;
+                //UnregisterButton.IsEnabled = BackgroundTaskSample.TimeTriggeredTaskRegistered;
+                //Progress.Text = BackgroundTaskSample.TimeTriggeredTaskProgress;
+                //Status.Text = BackgroundTaskSample.GetBackgroundTaskStatus(BackgroundTaskSample.TimeTriggeredTaskName);
+
+                logItems.Add(string.Format("{0} Background Task completed", DateTime.Now.ToString()));
+
+           });
+        }
+
+        private void ResetLogButton_Click(object sender, RoutedEventArgs e)
+        {
+            logItems.Clear();
+        }
+    }
+}
---
+++ AppServices/cs/BGTaskRuntimeComponent/SampleBackgroundTask.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.AppService;
+using Windows.ApplicationModel.Background;
+using Windows.Data.Xml.Dom;
+using Windows.Foundation.Collections;
+using Windows.UI.Notifications;
+
+namespace BGTaskRuntimeComponent
+{
+    public sealed class SampleBackgroundTask : IBackgroundTask
+    {
+        BackgroundTaskDeferral _deferral;
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            _deferral = taskInstance.GetDeferral();
+
+            StringBuilder sbToastContent = new StringBuilder();
+
+            using (var connection = new AppServiceConnection())
+            {
+
+                //Set up a new app service connection
+                connection.AppServiceName = "com.microsoft.randomnumbergenerator";
+                connection.PackageFamilyName = "Microsoft.SDKSamples.AppServicesProvider.CS_8wekyb3d8bbwe";
+                AppServiceConnectionStatus status = await connection.OpenAsync();
+
+                var bCanContinue = false;
+
+                switch (status)
+                {
+                    case AppServiceConnectionStatus.Success:
+                        // The new connection opened successfully
+                        sbToastContent.AppendLine("Connection established.");
+                        bCanContinue = true;
+                        break;
+
+                    case AppServiceConnectionStatus.AppNotInstalled:
+                        sbToastContent.AppendLine("The app AppServicesProvider is not installed. Deploy AppServicesProvider to this device and try again.");
+                        break;
+
+                    case AppServiceConnectionStatus.AppUnavailable:
+                        sbToastContent.AppendLine("The app AppServicesProvider is not available. This could be because it is currently being updated or was installed to a removable device that is no longer available.");
+                        break;
+
+                    case AppServiceConnectionStatus.AppServiceUnavailable:
+                        sbToastContent.AppendLine("The app AppServicesProvider is installed but it does not provide the app service" );
+                        break;
+
+                    default:
+                    case AppServiceConnectionStatus.Unknown:
+                        sbToastContent.AppendLine("An unknown error occurred while we were trying to open an AppServiceConnection.");
+                        break;
+                }
+
+                if (bCanContinue)
+                {
+                    //Set up the inputs and send a message to the service
+                    var inputs = new ValueSet();
+                    inputs.Add("minvalue", 0);
+                    inputs.Add("maxvalue", 10);
+                    AppServiceResponse response = await connection.SendMessageAsync(inputs);
+
+                    //If the service responded with success display the result and walk away
+                    if (response.Status == AppServiceResponseStatus.Success &&
+                        response.Message.ContainsKey("result"))
+                    {
+                        var resultText = response.Message["result"].ToString();
+                        if (!string.IsNullOrEmpty(resultText))
+                        {
+                            //Result.Text = resultText;
+                            sbToastContent.AppendLine(string.Format("App service responded with value {0}", resultText));
+                        }
+                        else
+                        {
+                            sbToastContent.AppendLine("App service did not respond with a result");
+                        }
+
+                        //return;
+                    }
+                    else
+                    {
+                        //Something went wrong while sending a message. Let display
+                        //a meaningful error message
+                        switch (response.Status)
+                        {
+                            case AppServiceResponseStatus.Failure:
+                                sbToastContent.AppendLine("The service failed to acknowledge the message we sent it. It may have been terminated or it's RequestReceived handler might not be handling incoming messages correctly.");
+                                break;
+
+                            case AppServiceResponseStatus.ResourceLimitsExceeded:
+                                sbToastContent.AppendLine("The service exceeded the resources allocated to it and had to be terminated.");
+                                break;
+
+                            default:
+                            case AppServiceResponseStatus.Unknown:
+                                sbToastContent.AppendLine("An unknown error occurred while we were trying to send a message to the service.");
+                                break;
+                        }
+                    }
+                   
+                }
+                else
+                {
+                    sbToastContent.AppendLine("App Service is unavailable");
+                }
+
+               
+            }
+
+            // Send local Toast notification
+
+            PopToast(sbToastContent.ToString());
+
+            _deferral.Complete();
+        }
+
+
+        private void PopToast(string text)
+        {
+            // create and send local notificatio  with new app version
+            var xmlString = string.Format(@"
+                            <toast>  
+                                <visual>
+                                    <binding template='ToastGeneric'>                                        
+                                        <text>BG task result: {0}</text>
+                                    </binding>      
+                                </visual>
+                            </toast>",
+                            text);
+
+            var longTime = new Windows.Globalization.DateTimeFormatting.DateTimeFormatter("longtime");
+            DateTimeOffset expiryTime = DateTime.Now.AddSeconds(8);
+            string expiryTimeString = longTime.Format(expiryTime);
+
+            XmlDocument toastXml = new XmlDocument();
+            toastXml.LoadXml(xmlString);
+
+            // Set the duration on the toast
+            IXmlNode toastNode = toastXml.SelectSingleNode("/toast");
+            ((XmlElement)toastNode).SetAttribute("duration", "long");
+
+            // Create the actual toast object using this toast specification.
+            ToastNotification toast = new ToastNotification(toastXml);
+            toast.ExpirationTime = expiryTime;
+
+            // Send the toast
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
+        }
+
+      
+    }
+}
